692f52f7f1a6c68cf874f94a2079389d8bd30e8d
ActuallyGiggles <95275195+ActuallyGiggles@users.noreply.github.com>
2022-09-22 03:09:33 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,107 @@
+let DEBUG = false;
+
+let channelName = "39daph";
+let message = "";
+
+const url = "https://actuallygiggles.localtonet.com/getsentence?channel=";
+
+const onReady = (callback) => {
+	if (document.readyState != "loading") {
+		callback();
+	}
+	else if (document.addEventListener) {
+		document.addEventListener("DOMContentLoaded", callback);
+	}
+	else {
+		document.attachEvent("onreadystatechange", function() {
+			if (document.readyState == "complete") {
+				callback();
+			}
+		});
+	}
+};
+
+// https://stackoverflow.com/questions/111529/how-to-create-query-parameters-in-javascript
+function encodeQueryData(data) { 
+    const ret = [];
+    for (let d in data) {
+        if (data[d]) {
+            ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+		}
+    }
+    return ret.join('&');
+}
+
+const getJson = (url) => fetch(url, { method: "GET" }).then(async (response) => {
+	const contentType = response.headers.get("Content-Type");
+	if (contentType.includes("text/plain")) {
+		const text = await response.text();
+		return text;
+	} else if (contentType.includes("application/json")) {
+		return await response.json();
+	}
+}).catch((error) => console.error(error));
+
+async function fetchMarkovMessage(event) {
+	if (event != null) {
+		event.preventDefault();
+	}
+
+	result.classList.add("hidden");
+	result.textContent = "";
+	loading.classList.remove("hidden");
+	
+	channelName = channel.value;
+
+	let urlParameters = {};
+	urlParameters["channel"] = channelName;
+
+	if (DEBUG) {
+		urlParameters["debug"] = true;
+	}
+
+	let urlParameterString = `${window.location.pathname}?${encodeQueryData(urlParameters)}`;
+	window.history.pushState(null, "", urlParameterString);
+
+	// const json = JSON.parse(await getJson(`${url}${channelName}`));
+	const json = await getJson(`${url}${channelName}`);
+
+	if(json == undefined) {
+		message = "Something went wrong!"
+	}
+	else if (json.error != "") {
+		message = json["error"];
+	}
+	else {
+		message = json["markov_sentence"];
+	}
+
+	generateHtml();
+}
+
+function generateHtml() {
+	const messageNode = document.createTextNode(message);
+	result.appendChild(messageNode);
+
+	loading.classList.add("hidden");
+	result.classList.remove("hidden");
+}
+
+const generator = document.getElementById("generator");
+const channel = document.getElementById("channel");
+const loading = document.getElementById("loading");
+const result = document.getElementById("result");
+
+generator.addEventListener("submit", (event) => fetchMarkovMessage(event));
+
+onReady(() => { 
+	const searchParameters = new URLSearchParams(window.location.search);
+	if(searchParameters.get("debug")) {
+		DEBUG = true;
+	}
+
+	if(searchParameters.has("channel")) {
+		channel.value = searchParameters.get("channel");
+		fetchMarkovMessage(null);
+	}
+});
